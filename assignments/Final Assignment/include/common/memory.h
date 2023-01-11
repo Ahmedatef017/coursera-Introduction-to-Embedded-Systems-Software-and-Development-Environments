44cd9b0961652c25a3b04c6c9e83137d2be48302
Ahmedatef017 <85120938+Ahmedatef017@users.noreply.github.com>
2023-01-11 23:06:40 +0200

    Update memory.h

--- assignments/Final Assignment/include/common/memory.h
+++ assignments/Final Assignment/include/common/memory.h
@@ -1,25 +1,4 @@
-/******************************************************************************
- * Copyright (C) 2017 by Alex Fosdick - University of Colorado
- *
- * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
- * permitted to modify this and use it to learn about the field of embedded
- * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
- *
- *****************************************************************************/
-/**
- * @file memory.h
- * @brief Abstraction of memory read, write, and manipulation operations
- *
- * This header file provides an abstraction of reading,
- * writing, and memory manipulation via function calls. 
- *
- * @author Alex Fosdick
- * @date 1/4/2017
- * @edited 24/10/2020 by Mohammed Abdelalim
- *
- */
+
 #ifndef __MEMORY_H__
 #define __MEMORY_H__
 
